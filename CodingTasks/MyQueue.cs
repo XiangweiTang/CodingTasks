bf937d286fefadd2fd7d7922f7e9739af6007bd5
Xiangwei Tang <tangxiangwei@gmail.com>
2022-02-17 10:43:51 +0800

    Add sample code for queue.

--- CodingTasks/MyQueue.cs
+++ CodingTasks/MyQueue.cs
@@ -8,12 +8,21 @@ namespace CodingTasks
 {
     class MyQueue
     {
-        public MyQueue()
+        private int[] InternalArray = new int[0];
+        public MyQueue():this(16)
         {
             
         }
 
+        public MyQueue(int capacity)
+        {
+            InternalArray = new int[capacity];
+            Count = 0;
+            HeadIndex = 0;
+        }
+
         public int Count { get; private set; }
+        private int HeadIndex = 0;
 
         /// <summary>
         /// Add element to the tail of the queue. Takes O(1) time.
@@ -22,7 +31,12 @@ namespace CodingTasks
         /// <exception cref="NotImplementedException"></exception>
         public void Enqueue(int value)
         {
-            throw new NotImplementedException();
+            if (Count == InternalArray.Length)
+                throw new Exception("Queue overflow.");
+            if (Count > InternalArray.Length)
+                throw new Exception("Queue broken.");
+            InternalArray[Tail()] = value;
+            Count++;
         }
 
         /// <summary>
@@ -32,7 +46,11 @@ namespace CodingTasks
         /// <exception cref="NotImplementedException"></exception>
         public int Dequeue()
         {
-            throw new NotImplementedException();
+            int peak = Peak();
+            HeadIndex++;
+            if (HeadIndex >= InternalArray.Length)
+                HeadIndex -= InternalArray.Length;
+            return peak;
         }
 
         /// <summary>
@@ -42,7 +60,23 @@ namespace CodingTasks
         /// <exception cref="NotImplementedException"></exception>
         public int Peak()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+                throw new Exception("Queue underflow.");
+            if (Count < 0 || HeadIndex < 0 || HeadIndex >= InternalArray.Length)
+                throw new Exception("Queue broken");
+            return InternalArray[HeadIndex];
+        }
+
+        private int Tail()
+        {
+            int tail = HeadIndex + Count;
+            if (tail < 0)
+                throw new Exception("Queue broken.");
+            if (tail >= InternalArray.Length)
+                tail -= InternalArray.Length;
+            if (tail >= InternalArray.Length)
+                throw new Exception("Queue broken.");
+            return tail;
         }
     }
 }
